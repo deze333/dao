ea5aeb3227fed6d2845f05193c6f8607a883cdde
Denis Zubkov <deze333@yahoo.com>
2014-07-23 23:51:31 -0700

    delete many fixes

--- dao_delete.go
+++ dao_delete.go
@@ -11,17 +11,30 @@ import (
 // DAO delete methods
 //------------------------------------------------------------
 
-// Deletes object from collection.
+// Deletes single object from collection.
 func (dao *DAO) Delete(id bson.ObjectId) (err error) {
 
 	err = dao.Coll.RemoveId(id)
 	return
 }
 
+// Deletes many objects by IDs.
+func (dao *DAO) DeleteMany(ids []bson.ObjectId) (count int, err error) {
+
+	q := M{"_id": M{"$in": ids}}
+
+	var info *mgo.ChangeInfo
+	info, err = dao.Coll.RemoveAll(q)
+	if info != nil {
+		count = info.Removed
+	}
+	return
+}
+
 // Deletes object from collection by given criteria.
 // Kvals is an array of key-value pairs like so:
 // "name", "Joe", "age", 99, ...
-func (dao *DAO) DeleteBy(kvals ...interface{}) (err error) {
+func (dao *DAO) DeleteBy(kvals ...interface{}) (count int, err error) {
 
 	q := M{}
 
@@ -30,17 +43,21 @@ func (dao *DAO) DeleteBy(kvals ...interface{}) (err error) {
 		q[fmt.Sprint(kvals[i])] = kvals[i+1]
 	}
 
-	err = dao.Coll.Remove(q)
+	var info *mgo.ChangeInfo
+	info, err = dao.Coll.RemoveAll(q)
+	if info != nil {
+		count = info.Removed
+	}
 	return
 }
 
 // Deletes all objects from collection.
-func (dao *DAO) DeleteAll(are, you, sure bool) (deleted int, err error) {
+func (dao *DAO) DeleteAll(are, you, sure bool) (count int, err error) {
 
 	var info *mgo.ChangeInfo
 	info, err = dao.Coll.RemoveAll(M{})
 	if info != nil {
-		deleted = info.Removed
+		count = info.Removed
 	}
 	return
 }
